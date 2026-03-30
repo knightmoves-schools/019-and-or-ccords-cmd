1ca06b2186706de7fc37414885d2fce77a5728b8
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 13:12:29 -0500

    Update Verifier methods with parameter types
    Refactor methods to include type specifications for parameters.

--- Verifier.cs
+++ Verifier.cs
@@ -1,12 +1,21 @@
 ﻿namespace knightmoves;
 public class Verifier
 {
-public string taxable(state, taxExempt) {
+public string taxable(string state, bool taxExempt) {
   if (state == 'IA' && taxExempt == false) {
     return "is taxable";
-  }};
-public string isVehicle(hasWheels, canFly, canSwim) {
+  }}
+public string isVehicle(bool hasWheels, bool canFly, bool canSwim) {
   if (hasWheels == true || canFly == true || canSwim == true) {
     return "is vehicle";
-  }};
+  }}
 }
+/*should create a method named Taxable that takes two parameters state and taxExempt and returns the string "is taxable" if the state is IA and taxExempt is false
+should return an empty string ("") from Taxable if the state is IA and taxExempt is true
+should return an empty string ("") from Taxable if the state is FL and taxExempt is false
+should create a method named IsVehicle that takes three parameters hasWheels, canFly, canSwim and returns the string "is vehicle" if at least one of the three parameters evaluates to true
+should return the string "is vehicle" from IsVehicle when hasWheels is true and canFly and canSwim are both false
+should return the string "is vehicle" from IsVehicle when canFly is true and hasWheels and canSwim are both false
+should return the string "is vehicle" from IsVehicle when canSwim is true and canFly and hasWheels are both false
+should return the string "is vehicle" from IsVehicle when canSwim and canFly are both true and hasWheels is false
+should return an empty string ("") from IsVehicle when hasWheels, canFly, and canSwim are all false
