6131c5d2b039229b97d196859b967dc087f2ba93
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 13:13:48 -0500

    Update Verifier.cs

--- Verifier.cs
+++ Verifier.cs
@@ -10,12 +10,3 @@ public string isVehicle(bool hasWheels, bool canFly, bool canSwim) {
     return "is vehicle";
   }}
 }
-/*should create a method named Taxable that takes two parameters state and taxExempt and returns the string "is taxable" if the state is IA and taxExempt is false
-should return an empty string ("") from Taxable if the state is IA and taxExempt is true
-should return an empty string ("") from Taxable if the state is FL and taxExempt is false
-should create a method named IsVehicle that takes three parameters hasWheels, canFly, canSwim and returns the string "is vehicle" if at least one of the three parameters evaluates to true
-should return the string "is vehicle" from IsVehicle when hasWheels is true and canFly and canSwim are both false
-should return the string "is vehicle" from IsVehicle when canFly is true and hasWheels and canSwim are both false
-should return the string "is vehicle" from IsVehicle when canSwim is true and canFly and hasWheels are both false
-should return the string "is vehicle" from IsVehicle when canSwim and canFly are both true and hasWheels is false
-should return an empty string ("") from IsVehicle when hasWheels, canFly, and canSwim are all false*/ 
