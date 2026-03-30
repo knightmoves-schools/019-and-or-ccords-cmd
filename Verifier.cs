b1e5cafe97e08b02ea8732e5e133175e50ed30d3
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 13:16:08 -0500

    Update Verifier.cs

--- Verifier.cs
+++ Verifier.cs
@@ -1,11 +1,11 @@
 ﻿namespace knightmoves;
 public class Verifier
 {
-public string taxable(string state, bool taxExempt) {
+public string Taxable(string state, bool taxExempt) {
   if (state == 'IA' && taxExempt == false) {
     return "is taxable";
   }}
-public string isVehicle(bool hasWheels, bool canFly, bool canSwim) {
+public string IsVehicle(bool hasWheels, bool canFly, bool canSwim) {
   if (hasWheels == true || canFly == true || canSwim == true) {
     return "is vehicle";
   }}
